2a7845c24b126067b6fa231f2c412821b45c0d79
githubuiux <uiuxista@gmail.com>
2023-11-14 12:32:00 +0300

    Initial Commit

---
+++ ReelzVideoLayout/ReelzVideoLayoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReelzVideoLayoutApp.swift
+//  ReelzVideoLayout
+//
+//  Created by Levent on 14.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ReelzVideoLayoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
